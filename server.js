1e305f658eaf392d410fae2b98d57631e23da5af
tparlmer <tparlmer@gmail.com>
2022-05-10 19:00:29 -0400

    added server.js functionality

--- server.js
+++ server.js
@@ -5,30 +5,36 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // require sequelize
-    // setup from config connection.js file
+// setup from config connection.js file
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 // Setup sess for hashing and passwords
 const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-      db: sequelize
-    })
-  };
-  
-  app.use(session(sess));
+  secret: "Super secret secret",
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 // set up helpers.js for formatting time and date (timestamps)
-const helpers = require('./utils/helpers');
+const helpers = require("./utils/helpers");
 
 const hbs = exphbs.create({ helpers });
 
 // call handlebars
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 // call express
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
-// node refernce to controllers folder
-app.use(require('./controllers/'));
-
+// node reference to controllers folder
+app.use(require("./controllers/"));
