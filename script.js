beac852b25f40a3c58afb009cc32ee97bfcfe4fa
megparlmer <megparlmer@gmail.com>
2022-05-11 22:36:58 -0400

    semi functional quiz

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //establishing start button and question container
+
 const startButton = document.getElementById('start-btn');
 const nextButton = document.getElementById('next-btn');
 const questionContainerElement = document.getElementById('question-container')
@@ -9,6 +10,10 @@ let shuffledQuestions, currentQuestionIndex
 
 //event listener for start button
 startButton.addEventListener('click', startTest)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion();
+})
 
 //start test function
 function startTest() {
@@ -42,18 +47,45 @@ function showQuestion(question) {
 }
 
 function resetState() {
+    clearStatusClass(document.body)
+    //hides next button as user moves through test
     nextButton.classList.add('hide')
     while (answerButtonsElement.firstChild) {
-        answerButtonsElement.removeChild
-        (answerButtonsElement.firstChild)
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
     }
 }
 
 //select answer function
 //passing event through as parameter
 function selectAnswer(e) {
-
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
 }
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
 
 const questions = [
     {
@@ -61,6 +93,13 @@ const questions = [
         answers: [
             { text: 'A', correct: true },
             { text: 'F', correct: false }
+        ]
+    },
+    {
+        question: 'What does "Bus" start with?',
+        answers: [
+            { text: 'L', correct: false },
+            { text: 'B', correct: true }
         ]
     }
 ]
