930721df32675afd1be9b7b41f493d21a58b4f19
tparlmer <tparlmer@gmail.com>
2022-05-12 12:27:55 -0400

    created column values for Quiz.js model

--- models/Quiz.js
+++ models/Quiz.js
@@ -8,6 +8,137 @@ Quiz.init(
     {
         // Table Column definitions go here (see 13.1.5 for reference)
         // TODO: add all necessary values to store quiz data from frontend
+        
+        // A
+        question1: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // B
+        question2: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // C
+        question3: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // D
+        question4: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // E
+        question5: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // F
+        question6: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // G
+        question7: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // H
+        question8: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // I
+        question9: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // J
+        question10: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // K
+        question11: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // L
+        question12: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // M
+        question13: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // N
+        question14: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // O
+        question15: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // P
+        question16: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Q
+        question17: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // R
+        question18: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // S
+        question19: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // T
+        question20: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // U
+        question21: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // V
+        question22: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // W
+        question23: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // X
+        question24: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Y
+        question25: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Z
+        question26: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
     },
     {
         // Table configuration options go here
