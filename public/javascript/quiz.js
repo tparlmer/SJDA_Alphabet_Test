8c4df5542f86d689a7982b1e8dffa6f388424835
Brittany Ganser <15brganser@gmail.com>
2022-05-11 21:47:34 -0600

    pseudocode

---
+++ public/javascript/quiz.js
@@ -0,0 +1,16 @@
+//user logs in using username (and password?) login screen
+////////if logged in
+//click start button and start quiz
+//start quiz begins questions 1-26
+////begin question object "What letter does this begin with?"
+////show emoji
+////user inputs letter
+////check for correct answer
+////if correct answer, add +1 to score variable
+////if false do not increase score
+////next question
+////repeat until letter z
+//at end of test, show a finish page
+//Ask to retake or test another user
+//if retake start test over
+//if test another user redirect to login page
