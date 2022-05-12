e96ab2959f11f5f503484049b2ce7157f31b40f8
Brittany Ganser <15brganser@gmail.com>
2022-05-11 22:36:33 -0600

    added emoji images

--- public/javascript/quiz.js
+++ public/javascript/quiz.js
@@ -14,3 +14,64 @@
 //Ask to retake or test another user
 //if retake start test over
 //if test another user redirect to login page
+
+// A - apple
+// B - bike
+// C - cat
+// D - duck
+// E - egg
+// F - fish
+// G - goat
+// H - house
+// I - iguana
+// J - juice
+// K - kite
+// L - lion
+// M - mouse
+// N - nose
+// O - octopus
+// P - panda
+// Q - quilt
+// R - rope
+// S - sandwich
+// T - television
+// U - umbrella
+// V - violin
+// W - wagon
+// X - xylophone - NEED STILL
+// Y - yo-yo
+// Z - zebra
+
+let studentScore;
+
+const correctAnswers = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+const questions = [{ image: "./public/images/aImage.svg" }];
