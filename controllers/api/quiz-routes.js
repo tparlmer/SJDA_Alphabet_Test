1b1f9d55216aaf6bc1fe83f7e471c232b4add000
tparlmer <tparlmer@gmail.com>
2022-05-12 20:35:28 -0400

    successfully tested quiz routes

--- controllers/api/quiz-routes.js
+++ controllers/api/quiz-routes.js
@@ -5,20 +5,30 @@ const { Quiz } = require('../../models')
 
 // TODO: Add CRUD operations for Quiz routes
 
+// GET request can send data to the frontend, but cannot collect it
 // GET /api/quiz
 router.get('/', (req, res) => {
     // gets quizzes of every user in the database
-    res.json()
+    console.log("Merry xmas ya filthy animal")
+    res.json();
 })
 
 // GET /api/quiz/1
 router.get('/:id', (req, res) => {
     // gets a specific quiz
+    // figure out how to tell database that the student has completed a specific quiz
+    console.log("meh")
+    res.send();
 })
 
 // POST /api/quiz
 router.post('/', (req, res) => {
     // creates a quiz in the quiz table (this will be a row of results)
+    // take variable where you stored the quiz results on the frontend
+    // returns something back to the frontend
+    // GOAL - GET JSON FROM FRONTEND
+    console.log("meoww");
+    res.send();
 })
 
 // must export rotuer for the file/ routes to be readable - like a return statement in a function
