798fe63629cf5fa7983660c7f6d5cd9fe023a27a
colinc27 <colinchristoph27@gmail.com>
2022-05-14 05:48:30 -0700

    Added index.js to seed folder

---
+++ seeds/index.js
@@ -0,0 +1,18 @@
+const seedUsers = require('./user-seeds');
+const seedUsers = require('./25-word-list');
+
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
+
+  //await seed25-word-list();
+  //console.log('--------------');
+  process.exit(0);
+};
+
+seedAll();
