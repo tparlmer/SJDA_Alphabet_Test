1f6b502ab3b96d6eacfd96fd47b8b103cc8b7042
megparlmer <megparlmer@gmail.com>
2022-05-16 10:33:27 -0400

    pre login functionality

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,18 @@
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector('.form-message');
+
+    messageElement.textContent = message;
+    messageElement.classList.remove('form-message-success', 'form-message-error');
+    messageElement.classList.add(`form-message-${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add('form-input-error');
+    inputElement.parentElement.querySelector('.form-input-error-message').textContent = message;
+}
+
+setFormMessage(loginForm, 'success', 'You are logged in:)');
+
 document.addEventListener('DOMContentLoaded', () => {
     const loginForm = document.querySelector('#login');
     const createAccountForm = document.querySelector('#create-account');
@@ -13,4 +28,18 @@ document.addEventListener('DOMContentLoaded', () => {
         loginForm.classList.remove('form-hidden');
         createAccountForm.classList.add('form-hidden');
     });
+
+    loginForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        //perform your login
+
+        setFormMessage(loginForm, 'error', 'invalid username/password');
+    });
+
+    document.querySelectorAll('.form-input').forEach(inputElement => {
+        inputElement.addEventListener('blur', e => {
+            
+        })
+    })
 })
